5d029b714d359a4e4d430645fdde5d348b03ecca
Jake Albaugh <jake.albaugh@gmail.com>
2016-05-28 22:14:26 -0500

    correct name

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -2,7 +2,7 @@ class WallController < ApplicationController
   before_action :twitter_client
 
   def index
-    search = TweetsRequest.new.request('@jake_albaugh')
+    search = TweetsRequest.new.request('@jakes_wall')
     tweets = @twitter.search(search, result_type: 'mixed').take(100)
     @tweets = TweetsPackage.new(tweets).package
     render json: @tweets
